221bc8c8e502bf6b8d9155ccee7136154bbcbc66
Erica Collier <ecollier871@outlook.com>
2024-09-10 11:14:59 -0400

    Finishedgit add .git add . Everything is in working order as required per the project

--- force-app/main/default/lwc/billList/billList.js
+++ force-app/main/default/lwc/billList/billList.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getBills from '@salesforce/apex/BillAPI.getBills';
 
-export default class BillList extends LightningElement {}
+export default class BillList extends LightningElement {
+    @track bills = [];
+    @track error;
+    searchKey = '';
+
+    @wire(getBills, { searchKey: '$searchKey' })
+    wiredBills({ data, error }) {
+        if (data) {
+            this.bills = data;
+            this.error = undefined;
+        }  else if (error) {
+            // Extracting the error message from the error object
+            console.log('Error object:', JSON.stringify(error));  // Log the full error object for inspection
+            this.error = error.body ? error.body.message : error.message;
+            this.bills = [];
+        
+        }
+    }
+
+    handleSearchChange(event) {
+        window.clearTimeout(this.delayTimeout);
+        const searchValue = event.target.value;
+        this.delayTimeout = setTimeout(() => {
+            this.searchKey = searchValue;
+        }, 300);
+    }
+}
+
+
+//Salesforce CLI command to deploy
+// sfdx force:source:deploy -p force-app/main/default
